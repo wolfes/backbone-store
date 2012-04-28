762e1f926a03551602174105077cc6be8efeae3c
Michael R Stunes <stunes@mit.edu>
2012-04-28 14:04:48 -0400

    Can load models from cache

--- cached_model.js
+++ cached_model.js
@@ -14,16 +14,27 @@ var destroy_fn = function(m) {
 
 // Constructor for a CachedModel object
 var CachedModel = Backbone.Model.extend({
+    defaults: {"my_class" : "CachedModel"},
+
     constructor: function(cache_id) {
         console.debug("Entering CachedModel.constructor");
 
         // Invoke Model's own constructor first
         Backbone.Model.prototype.constructor.apply(this);
 
-        if (cache_id) {
-            this.set({cache_id: cache_id});
-        } else {
+        // Check for provided cache_id and generate one if not given one
+        if (!cache_id) {
             this.set({cache_id: createGUID()});
+        } else {
+            // If given an ID, see if it's already present in local storage
+            if (localStorage.hasOwnProperty(cache_id)) {
+                // If so, load all of its old attributes
+                var old_object = JSON.parse(localStorage[cache_id]);
+                this.set(old_object);
+            } else {
+                // If not already present, make a new model object
+                this.set({cache_id: cache_id});
+            }
         }
 
         // Attach listeners for persisting and destroying
@@ -36,3 +47,17 @@ var CachedModel = Backbone.Model.extend({
         console.debug("Exiting CachedModel.constructor");
     },
 });
+
+
+var MyAwesomeModel = CachedModel.extend({
+    defaults: {"my_class" : "MyAwesomeModel"},
+
+    initialize: function() {
+        console.debug("MyAwesomeModel.initialize");
+    },
+});
+
+
+// TODO: also allow passing attributes in constructor?
+// TODO: what if subclass's initialize clobbers cached attrs?
+//   avoid running initialize when loading from cache?
