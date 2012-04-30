c3cfb7f17b4ce75fef9ca2bbbafe8af5e2737219
Mike Stunes <kc0tqx@gmail.com>
2012-04-30 15:19:23 -0400

    Support for loading a model from cache, and passing attrs in xtor

--- cached_model.js
+++ cached_model.js
@@ -7,6 +7,7 @@ var update_fn = function(m) {
     localStorage.setItem(m.get("cache_id"), JSON.stringify(m));
 };
 
+// Callback for removing a model from the local store
 var destroy_fn = function(m) {
     localStorage.removeItem(m.get("cache_id"));
 };
@@ -14,50 +15,74 @@ var destroy_fn = function(m) {
 
 // Constructor for a CachedModel object
 var CachedModel = Backbone.Model.extend({
-    defaults: {"my_class" : "CachedModel"},
-
-    constructor: function(cache_id) {
+    constructor: function(attributes, options) {
         console.debug("Entering CachedModel.constructor");
 
         // Invoke Model's own constructor first
-        Backbone.Model.prototype.constructor.apply(this);
+        Backbone.Model.prototype.constructor.call(this, attributes, options);
 
-        // Check for provided cache_id and generate one if not given one
-        if (!cache_id) {
+        // Check if cache_id was given to us: if not, generate one
+        if (!this.get("cache_id")) {
             this.set({cache_id: createGUID()});
-        } else {
-            // If given an ID, see if it's already present in local storage
-            if (localStorage.hasOwnProperty(cache_id)) {
-                // If so, load all of its old attributes
-                var old_object = JSON.parse(localStorage[cache_id]);
-                this.set(old_object);
-            } else {
-                // If not already present, make a new model object
-                this.set({cache_id: cache_id});
-            }
         }
 
         // Attach listeners for persisting and destroying
-        this.on("change", function() {update_fn(this)});
-        this.on("destroy", function() {destroy_fn(this)});
+        this._set_listeners();
 
         // Save in cache
         update_fn(this);
 
         console.debug("Exiting CachedModel.constructor");
+        return this;
+    },
+
+
+    reload_hook: function() {},
+
+    _set_listeners: function() {
+        this.on("change", function() {update_fn(this)});
+        this.on("destroy", function() {destroy_fn(this)});
+    },
+});
+
+_.extend(CachedModel, {
+    from_cache: function(cache_id) {
+        // Check that we were given a cache_id, and that it's actually in the cache
+        // TODO: if ID not in cache, just make a new object?
+        if (!cache_id) return;
+        if (!localStorage.hasOwnProperty(cache_id)) return;
+
+        // Fetch old data from the cache before obliterating it by creating a new model
+        var old_attributes = JSON.parse(localStorage[cache_id]);
+
+
+        // Create a new model, using the usual initialize and constructor of
+        // whichever type the programmer wants
+        // It's on the programmer to call from_cache of the right constructor
+        // function
+        var new_model = new this({cache_id: cache_id});
+
+        // Then, reload all of the old attributes
+        new_model.set(old_attributes);
+
+        new_model.reload_hook();
+        // new_model should already have listeners set (by "new")
+        
+        return new_model;
     },
 });
 
 
+// Extension of CachedModel, for testing that the correct attributes all stick
+// around after reloading from cache
 var MyAwesomeModel = CachedModel.extend({
-    defaults: {"my_class" : "MyAwesomeModel"},
+    defaults: {"my_class" : "MyAwesomeModel", c:"e", f:"g"},
 
     initialize: function() {
         console.debug("MyAwesomeModel.initialize");
     },
+
+    validate: function(attrs) {
+        console.debug("MyAwesomeModel.validate");
+    },
 });
-
-
-// TODO: also allow passing attributes in constructor?
-// TODO: what if subclass's initialize clobbers cached attrs?
-//   avoid running initialize when loading from cache?
