771c7de6c949a94b24ce678132e751254fd3ebcd
Mike Stunes <kc0tqx@gmail.com>
2012-05-15 23:58:30 -0400

    Shortened IDs in cache

--- appserv/public/javascripts/utils.js
+++ appserv/public/javascripts/utils.js
@@ -7,7 +7,8 @@
 // We may consider replacing it with something better.
 // Source: http://stackoverflow.com/questions/6906916/collisions-when-generating-uuids-in-javascript
 var createGUID = function() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    //return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
     });
