8dcf42666183be94a2bd6266453c13945255a850
Wolfe Styke <wstyke@mit.edu>
2012-05-16 02:28:58 -0400

    fixed ajax href -> origin

--- appserv/public/javascripts/ajax.js
+++ appserv/public/javascripts/ajax.js
@@ -8,7 +8,7 @@
 var ajax = ajax || {};
 var util = util || {};
 
-ajax.baseURL = window.location.href;
+ajax.baseURL = window.location.origin;
 
 ajax.getDocs = function(continuation) {
   ajax.getJSON('data/docs', continuation);
